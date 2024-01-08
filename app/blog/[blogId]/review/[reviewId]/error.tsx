9e496672c7c3ff1ac25a0b8adc070288268bdfc7
Apurv gupta <apurvgupta.124@gmail.com>
2024-01-08 19:24:44 +0530

    ** Parallel routing (slots)

--- app/blog/[blogId]/review/[reviewId]/error.tsx
+++ app/blog/[blogId]/review/[reviewId]/error.tsx
@@ -1,8 +1,20 @@
 "use client";
 import React from "react";
 
-const ErrorBoundary = ({ error }: { error: Error }) => {
-  return <div>Error in reviwe {error.message}</div>;
+const ErrorBoundary = ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <h2>{error.message}</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
 };
 
 export default ErrorBoundary;
