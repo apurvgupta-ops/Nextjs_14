67ecccffa73cb30f167dfd770453408f4ad44355
Apurv gupta <apurvgupta.124@gmail.com>
2024-01-08 12:07:35 +0530

    SEO metadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog",
+  },
+};
 const Blog = () => {
   return <div>Blog page</div>;
 };
--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,4 +1,25 @@
 import React from "react";
+import { Metadata } from "next";
+
+type props = {
+  params: {
+    slug: string[];
+  };
+};
+
+export const generateMetadata = async ({
+  params,
+}: props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${params.slug[0]}`);
+    });
+  });
+
+  return {
+    title: `product ${title}`,
+  };
+};
 
 const Products = ({ params }: any) => {
   console.log({ params });
